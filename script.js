17356d337b8968fe96dcdb629409396bf2bbffc6
lucdevasconcellosbr <lucdevasconcellosbr@gmail.com>
2026-03-11 23:43:25 -0300

    da

---
+++ script.js
@@ -0,0 +1,19 @@
+const links = document.querySelectorAll('nav a');
+
+links.forEach(link => {
+
+link.addEventListener('click', function(e){
+
+e.preventDefault();
+
+const id = this.getAttribute("href");
+
+document.querySelector(id).scrollIntoView({
+
+behavior: "smooth"
+
+});
+
+});
+
+});
