9dfb6ad0962dca4ecf03f833e79dfd7753d8ea36
Lucas Alexandre <lucdevasconcellosbr@gmail.com>
2026-03-12 00:57:47 -0300

    Add functionality to fetch and display repositories
    Fetch and display GitHub repositories in the UI.

--- script.js
+++ script.js
@@ -30,3 +30,25 @@ distance: '40px',
 duration: 800,
 origin: 'right'
 });
+
+fetch("https://api.github.com/users/lucdevasconcellosbr-source/repos")
+.then(res => res.json())
+.then(data => {
+
+const container = document.getElementById("repos-container");
+
+data.slice(0,6).forEach(repo => {
+
+const card = document.createElement("div");
+card.className = "card";
+
+card.innerHTML = `
+<h3>${repo.name}</h3>
+<p>${repo.description || "Projeto disponível no GitHub"}</p>
+<a href="${repo.html_url}" class="project-btn">Ver repositório</a>
+`;
+
+container.appendChild(card);
+
+});
+});
